2478d24e6ec688110c8996cb6e339e236093ef82
Theodore P <teopanta1986@gmail.com>
2020-07-31 19:57:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,113 @@
-var App = React.createClass({
+function twodGrid(rows) {
+  var arr = [];
+  for (var i = 0; i < rows; i++) {
+    arr[i] = [];
+  }
+  return arr;
+}
 
-  getInitialState: function() {
-    return {
+var arr = twodGrid(400);
+var arr2 = twodGrid(400);
+// arr[50][2] = 5;
+// console.log(arr);
+var textArray = ["X", "Y"];
+//var randomIndex = Math.floor(Math.random() * textArray.length);
+//var randomElement = textArray[randomIndex];
+var neighboor = [];
+tick(); //call main loop
 
-      color: "white"
-    };
-  },
-  move: function() {
-    var arr = [0, 1, 2, 4, 5, 6, 10, 11, 12, 19, 21, 22, 23, 25, 26];
-    var cell = document.getElementById("container").getElementsByTagName("td");
-    arr.sort(function() {
-      return .15 - Math.random();
-    })
-    var comp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-    var arrint = [];
-    var counter = 0;
-    var i=0;
-    for (i = 0; i < arr.length; i++) {
+//functions
+function tick() {
+  //main loop
+  drawGrid();
+  updateGrid();
+  requestAnimationFrame(tick);
+}
+function drawGrid() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
 
-      cell[arr[i]].style.backgroundColor = "red";
-      if (arr[i] === comp[i]) {
-        cell[comp[i]].style.backgroundColor = "red";
-      } else {
-       // cell[comp[i]].style.backgroundColor = "black";
+  ctx.clearRect(0, 0, 400, 400);
+  for (var i = 0; i < 400; i++) {
+    for (var j = 0; j < 400; j++) {
+      var randomIndex = Math.floor(Math.random() * textArray.length);
+      var randomElement = textArray[randomIndex];
+      arr[i][j] = randomElement;
+      if (arr[i][j] === "X") {
+        ctx.fillStyle = "#FF0000";
+
+        ctx.fillRect(i, j, 1, 1);
       }
     }
-  },
-  componentDidMount: function() {
-    var arr = [0, 1, 2, 4, 5, 6, 10, 11, 12, 19, 21, 22, 23, 25, 26];
-    var cell = document.getElementById("container").getElementsByTagName("td");
-    arr.sort(function() {
-      return .15 - Math.random();
-    })
-    var comp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-    var arrint = [];
-    var counter = 0;
-    var th = this;
-    setInterval(function() {
-      var c = counter++;
-      arrint.push(c)
-      for (var i = 0; i < arr.length; i++) {
-        if (comp[c] === arr[i]) {
-          th.move()
-          cell[comp[i]].style.backgroundColor = "black";
+  }
+}
+//array[i-1][j]
+//array[i][j-1]
+//array[i-1][j-1]
+//array[i+1][j]
+//array[i][j+1]
+//array[i+1][j+1]
+//array[i+1][j-1]
+//array[i-1][j+1]
+
+function updateGrid() {
+  //perform one iteration of grid update
+  for (var j = 1; j < arr - 1; j++) {
+    //iterate through rows
+    for (var k = 1; k < arr - 1; k++) {
+      //iterate through columns
+      var totalCells = 0;
+      //add up the total values for the surrounding cells
+      totalCells += arr[i - 1][j - 1]; //top left
+      totalCells += arr[j - 1][k]; //top center
+      totalCells += arr[j - 1][k + 1]; //top right
+
+      totalCells += arr[j][k - 1]; //middle left
+      totalCells += arr[j][k + 1]; //middle right
+
+      totalCells += arr[j + 1][k - 1]; //bottom left
+      totalCells += arr[j + 1][k]; //bottom center
+      totalCells += arr[j + 1][k + 1]; //bottom right
+
+      //apply the rules to each cell
+      if (arr[j][k] === 0) {
+        switch (totalCells) {
+          case 3:
+            arr2[j][k] = 1; //if cell is dead and has 3 neighbours, switch it on
+            break;
+          default:
+            arr2[j][k] = 0; //otherwise leave it dead
+        }
+      } else if (arr[j][k] === 1) {
+        //apply rules to living cell
+        switch (totalCells) {
+          case 0:
+          case 1:
+            arr2[j][k] = 0; //die of lonelines
+            break;
+          case 2:
+          case 3:
+            arr2[j][k] = 1; //carry on living
+            break;
+          case 4:
+          case 5:
+          case 6:
+          case 7:
+          case 8:
+            arr2[j][k] = 0; //die of overcrowding
+            break;
+          default:
+            arr2[j][k] = 0; //
         }
       }
-    }, 500);
-  },
-
-  render: function() {
-    var indents = [];
-    for (var i = 0; i < 5; i++) {
-      indents.push(<table style={{backgroundColor: this.state.color}} className='table' ref="td"><tr><td></td><td></td><td></td><td></td><td></td><td></td></tr></table>);
     }
-    return (
-      <div>
-        {indents}
-        <button onClick={this.getInitialState} >start</button>
-    </div>
-    );
   }
-});
 
-ReactDOM.render(
-  <App  />,
-  document.getElementById('container')
-);
+  for (var j = 0; j < 400; j++) {
+    //iterate through rows
+    for (var k = 0; k < 400; k++) {
+      //iterate through columns
+      arr[j][k] = arr2[j][k];
+    }
+  }
+}
